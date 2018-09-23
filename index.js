f8e640d3ae257a766d4fa19de9c43f43fab1a13e
PrivacyPolicy <gabehwebsites@gmail.com>
2018-09-23 00:51:14 +0000

    Init commit (basic quoted response)

---
+++ index.js
@@ -0,0 +1,43 @@
+const request = require('request');
+const express = require('express');
+const bodyParser = require('body-parser');
+const telegram = require('telegram-bot-api');
+const {token, port} = require('./env.js');
+
+const api = new telegram({ token });
+const app = express();
+app.use(bodyParser.json());
+
+app.post('/', (req, res) => {
+  console.log(req.body);
+  let message = getMessage(req.body);
+  let chatId = getChatId(req.body);
+  console.log('chat = ' + chatId + ', text = ' + message);
+  request({
+    url: `https://api.telegram.org/bot${token}/sendMessage`,
+    qs: { chat_id: chatId, text: `You said "${message}". HAHAHAHA Idiot.` }
+  }, function (err, res2, body) {
+    if (err) { console.log(err); res.sendStatus(400); return; }
+    console.log("We is good is: " + res2.body);
+    res.sendStatus(200);
+  });
+});
+
+app.listen(port, () => console.log(`Example app listening on port ${port}!`));
+
+function getMessage(body) {
+  try {
+    return body.message.text;
+  } catch (e) {
+    return '';
+  }
+}
+
+function getChatId(body) {
+  try {
+    return body.message.chat.id;
+  } catch (e) {
+    return '';
+  }
+}
+
